c64584132f6bcd7666dc3e84356cd07bffad7158
Andres Mena G <afmg19791130@hotmail.com>
2025-04-07 08:52:33 -0500

    Primera aplicacion con java

--- src/Desiciones.java
+++ src/Desiciones.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Desiciones {
+
+
+    public static void main(String[] args) {
+
+        int fechaDeLanzamiento=1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+        String tipoDePlan = "Plus";
+
+        if (fechaDeLanzamiento > 2022) {                       //operadores relacionales
+
+            System.out.println("Peliculas mas Populares");
+        } else {
+            System.out.println("Peliculas Retro que aun vale la pena ver");
+        }
+
+        if (incluidoEnElPlan == true || tipoDePlan.equals("Plus")) {  //operadores logicos
+
+            System.out.println("Disfrute de su pelicula");
+        } else {
+
+            System.out.println("Esta pelicula no esta incluida en su plan");
+        }
+
+
+
+
+
+
+    }
+
 }
--- src/Lectura.java
+++ src/Lectura.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Lectura {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in); // clase para ingresar datos
+
+        System.out.println("Escribe el nombre de tu pelicula favorita");
+
+        String pelicula = teclado.nextLine();
+
+        System.out.println("Ahora escribe la fecha de lanzamiento");
+
+        int fechaDeLanzamiento = teclado.nextInt();
+
+        System.out.println("Por ultimo dino que nota le das a esta pelicula");
+
+        double nota = teclado.nextDouble();
+
+        System.out.println(pelicula);
+        System.out.println(fechaDeLanzamiento);
+        System.out.println(nota);
+    }
 }
